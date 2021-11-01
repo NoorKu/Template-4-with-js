b430f33454e451c55c322ab44b47f6d5910a6c67
Noorku <noorskoulayb@gmail.com>
2021-10-31 22:36:36 -0700

    Make function for scrolling section to observe data-width

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let section = document.querySelector(".three");
+let spans = document.querySelectorAll(".progress span");
+window.onscroll = function() {
+    if(window.scrollY >= section.offsetTop) {
+    console.log("Reached Section Tree");
+    spans.forEach((span) => {
+        span.style.width = span.dataset.width;
+    })
+    }
+};
